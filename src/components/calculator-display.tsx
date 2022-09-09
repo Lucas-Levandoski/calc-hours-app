be02bd384a52ee89620786611bf32e15f904160f
Lucas Levandoski <lucas-levandoski@hotmail.com>
2022-09-08 22:29:10 -0300

    trying to fix the display size

--- src/components/calculator-display.tsx
+++ src/components/calculator-display.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { TextInput, View } from 'react-native';
 import { withTheme } from 'react-native-paper';
 import { Theme } from 'react-native-paper/lib/typescript/types';
+import calculatorDisplay from '../styles/calculator-display';
 
 type props = {
   theme: Theme;
@@ -9,7 +10,8 @@ type props = {
 
 const ReturnTypePadComponent = (props: props) => {
   return (
-    <View>
+    <View style={calculatorDisplay.display}>
+      testing stuff
       <TextInput
         multiline={true}
         value='test'
---
+++ src/styles/calculator-display.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  display: {
+    position: 'relative',
+    display: 'flex',
+    flexGrow: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'flex-end',
+    backgroundColor: 'blue',
+  },
+});
